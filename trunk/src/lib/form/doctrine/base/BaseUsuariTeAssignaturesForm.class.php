7bbfa61e309f33f8cea7d263bb5e23c970b3b325
nothingbuttumbleweed <nothingbuttumbleweed@e9757e0e-e334-8e11-19eb-ad52d53de0c3>
2011-01-11 20:57:46 +0000

    Acabat i configurat el pluguin sfDGP, ara falta que cadascú inserti els fixtures a la base de dades amb el comando: "symfony doctrine:data-load"

---
+++ trunk/src/lib/form/doctrine/base/BaseUsuariTeAssignaturesForm.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * UsuariTeAssignatures form base class.
+ *
+ * @method UsuariTeAssignatures getObject() Returns the current form's model object
+ *
+ * @package    ttupf
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseUsuariTeAssignaturesForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'             => new sfWidgetFormInputHidden(),
+      'usuari_id'      => new sfWidgetFormInputText(),
+      'assignatura_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Assignatura'), 'add_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'id'             => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'usuari_id'      => new sfValidatorInteger(),
+      'assignatura_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Assignatura'))),
+    ));
+
+    $this->widgetSchema->setNameFormat('usuari_te_assignatures[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'UsuariTeAssignatures';
+  }
+
+}
