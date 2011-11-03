7ac2a32b952d302123f3ad4bdda54df2a9149db7
nothingbuttumbleweed <nothingbuttumbleweed@gmail.com>
2011-11-03 19:48:33 +0000

    S'ha actualitzat el model, i les classes que li pertoquen. S'ha arreglat algun petit fallo a les fixtures.

--- src/apps/frontend/lib/sessionsTable.class.php
+++ src/apps/frontend/lib/sessionsTable.class.php
@@ -11,22 +11,22 @@ class sessionsTable
 	 * Conté el codi HTML de la taula
 	 */
 	private $table;
-	private $day_class;
+	private $date;
 
 	public function sessionsTable($sessions){
 		$this->sessionsArray = $sessions;
-		$this->haveClassesToday = false;
+		$haveClassesToday = false;
 
 		//Agafem com a date el dia actual
-		$date = date('d/m/Y', mktime(0, 0, 0, date('m')  , date('d'), date('Y')));
+		$this->date = date('d/m/Y', mktime(0, 0, 0, date('m')  , date('d'), date('Y')));
 	
 		//Agafem un dia determinat pel qual sabem que hi han sessions si no estem en l'entorn de debug:
 		if(sfConfig::get('sf_environment') == "dev") {
-		//	$date = date('d/m/Y', mktime(0, 0, 0, date(10)  , date(24), date(2011)));
+			$this->date = date('d/m/Y', mktime(0, 0, 0, date(10)  , date(24), date(2011)));
 		}
 
 		//TODO s'hauria de mostrar la data de les sessions de la taula; de moment mostra la data de la primera sessio del vector.
-		$this->table = "<table id='sessions'><thead><tr><th>".$date."</th></tr></thead>";
+		$this->table = "<table id='sessions'><thead><tr><th>".$this->date."</th></tr></thead>";
 		
 		foreach($this->sessionsArray as $session):
 			$dataHoraInici = $session->getDataHoraInici();
@@ -34,8 +34,8 @@ class sessionsTable
 			$session_date = date('d/m/Y', strtotime($dataHoraInici));
 			
 			//TODO seleccionar sessions del dia actual per defecte, permetre escollir altres dies.
-			if($session_date == $date) {
-				$this->haveClassesToday = true;
+			if($session_date == $this->date) {
+				$haveClassesToday = true;
 				$hora = explode(" ", $dataHoraInici);
 				$this->table .= "<tr><td>".$hora[1]."</td><td>".$session->getAssignatura()->getNom()."<br />".$session->getTipus()."<br />".$session->getAula()."</td></tr>";
 			}			
@@ -48,7 +48,7 @@ class sessionsTable
 		if($this->haveClassesToday) {
 			return $this->table;
 		} else {
-			return "Fantàstic! Tens el dia lliure.<br />";
+			return $this->date."<br />Fantàstic! Tens el dia lliure.<br />";
 		}
 	}
 }
