39873a61956ac32997148a403cc7b5941f85ea0b
juanrobertopescador <juanrobertopescador@gmail.com>
2011-11-07 00:39:58 +0000

    Afegit possibilitat d'especificar data en veure horari. Afegit enllaços de dia anterior/següent en veure horari. Canviat format d'especificar data, ara només és la data separada per barres: /horari/index/7/11/2011 per al 7 de novembre 2011. sessionsTable ja no fixa la data si és entorn dev, ja que es pot especificar.

--- src/apps/frontend/lib/sessionsTable.class.php
+++ src/apps/frontend/lib/sessionsTable.class.php
@@ -6,27 +6,29 @@
 //TODO en cas d'haver practiques i seminari alhora, mostrar les dues en una cel·la en lloc de dues files amb hora repetida.
 class sessionsTable
 {
+	private $logger;
 	private $sessionsArray;
 	/**
 	 * Conté el codi HTML de la taula
 	 */
 	private $table;
 	private $date;
-	private $hasClassesToday;
+	private $haveClassesToday;
 
 	public function sessionsTable($sessions){
+		$this->logger = SfContext::getInstance()->getLogger();
 		$this->sessionsArray = $sessions;
 		$this->haveClassesToday = false;
 
 		//Agafem com a date el dia actual
 		$this->date = date('d/m/Y', mktime(0, 0, 0, date('m')  , date('d'), date('Y')));
-	
-		//Agafem un dia determinat pel qual sabem que hi han sessions si no estem en l'entorn de debug:
-		if(sfConfig::get('sf_environment') == "dev") {
-//			$this->date = date('d/m/Y', mktime(0, 0, 0, date(10)  , date(24), date(2011)));
 		}
+}
 
-		//TODO s'hauria de mostrar la data de les sessions de la taula; de moment mostra la data de la primera sessio del vector.
+	/**
+	 * Genera el codi HTML de la taula d'horari
+	 */
+	public function generaHtml(){
 		$this->table = "<table id='sessions'><thead><tr><th>".$this->date."</th></tr></thead>";
 		
 		foreach($this->sessionsArray as $session):
@@ -34,15 +36,19 @@ class sessionsTable
 			
 			$session_date = date('d/m/Y', strtotime($dataHoraInici));
 			
-			//TODO seleccionar sessions del dia actual per defecte, permetre escollir altres dies.
 			if($session_date == $this->date) {
 				$this->haveClassesToday = true;
 				$hora = explode(" ", $dataHoraInici);
 				$this->table .= "<tr><td>".$hora[1]."</td><td>".$session->getAssignatura()->getNom()."<br />".$session->getTipus()."<br />".$session->getAula()."</td></tr>";
-			}			
+			}
+			else{
+				$this->logger->debug('Wrong day for session');
+				$this->logger->debug('session_date is: ' . $session_date . ' specified date is: ' . $this->date);
+			}
 		endforeach;
 		
 		$this->table .= "</table>";
+		$this->logger->debug("Html generated is: " . $this->table);
 	}
 
 	public function toString(){
@@ -51,5 +57,14 @@ class sessionsTable
 		} else {
 			return $this->date."<br />Fantàstic! Tens el dia lliure.<br />";
 		}
+	}
+
+	/**
+	 * Fixa la data a mostrar.
+	 *
+	 * @param String $date Data a mostrar en format mes/dia/any 
+	 */
+	public function setDate($date){
+		$this->date = $date;
 	}
 }
