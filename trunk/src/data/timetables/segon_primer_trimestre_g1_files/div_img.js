920d1fabeaa88807da5a4b2a685b81010412e72e
juanrobertopescador@gmail.com <juanrobertopescador@gmail.com@e9757e0e-e334-8e11-19eb-ad52d53de0c3>
2011-11-27 23:00:22 +0000

    Afegits fitxers html dels horaris amb què funciona ttupf per quan els treguin de la web de l'esup, si s'ha canviat totalment els horaris, al menys tenir el record dels bons temps.

---
+++ trunk/src/data/timetables/segon_primer_trimestre_g1_files/div_img.js
@@ -0,0 +1,5 @@
+<!--
+      		        if (!is_nav6up && !is_ie3 && !is_ie4 && !is_ie4up && !is_ie5up && !is_ie6){ 
+		           document.write('<img src="http://www.upf.edu/img/pt.gif" alt="&nbsp;" width="0" height="0">');
+		        }
+                        -->
