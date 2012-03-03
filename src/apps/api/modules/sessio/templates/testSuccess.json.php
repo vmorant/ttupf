90e9095e3404f9dfb912aacb0c5e4b5307d467e4
nothingbuttumbleweed <nothingbuttumbleweed@e9757e0e-e334-8e11-19eb-ad52d53de0c3>
2012-03-03 12:51:41 +0000

    Afegida informació de dates al routing de testSucces.json del modul api

--- src/apps/api/modules/sessio/templates/testSuccess.json.php
+++ src/apps/api/modules/sessio/templates/testSuccess.json.php
@@ -1,22 +1,24 @@
 <?php
 	$count = 0;
 	
-	$data["code"] = 0;
+	$json_data["code"] = 1;
+	$json_data["sessions"] = 0;
 	
-	if(count($sessionsArray) == 0) {
-		$data["code"] = 1;
-	} else {
-		foreach($sessionsArray as $session):
-				$dataHoraInici = $session->getDataHoraInici();
-				
-				$session_date = date('d/m/Y', strtotime($dataHoraInici));
-				
-				if($session_date == date('d/m/Y', mktime(0, 0, 0, date('m')  , date('d'), date('Y')))) {
-					$hora = explode(" ", $dataHoraInici);
-					$data["sessio".$count] = array("hora" => $hora[1], "assignatura" => $session->getAssignatura()->getNom(), "tipus" => $session->getTipus(), "aula" => $session->getAula());
-					$count++;
-				}
-		endforeach;
+	foreach($sessionsArray as $session):
+			$dataHoraInici = $session->getDataHoraInici();
+			
+			$session_date = date('d/m/Y', strtotime($dataHoraInici));
+
+			if($session_date == $data) {
+				$hora = explode(" ", $dataHoraInici);
+				$json_data["sessio".$count] = array("hora" => $hora[1], "assignatura" => $session->getAssignatura()->getNom(), "tipus" => $session->getTipus(), "aula" => $session->getAula());
+				$count++;
+			}
+	endforeach;
+	
+	if($count != 0) {
+		$json_data["code"] = 0;
+		$json_data["sessions"] = $count;
 	}
 	
-	echo json_encode($data);
+	echo json_encode($json_data);
